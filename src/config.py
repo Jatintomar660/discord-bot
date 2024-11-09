6b36ace92f5dcd43cf18f00ade45ef363a0fe350
jatintomar660 <jatintomar660@gmail.com>
2024-11-09 18:25:00 +0530

    updated config.py

--- src/config.py
+++ src/config.py
@@ -9,4 +9,4 @@ LEETCODE_API_BASE_URL= os.environ.get('LEETCODE_API','http://localhost:3003')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 CHANNEL_ID =  int(os.environ.get('CHANNEL_ID')) 
 
-SLEEP_TIME=3600
+SLEEP_TIME=4*3600
